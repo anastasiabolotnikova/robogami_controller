e05c6a3796b543c299d4a849d0e9f8c5c914b512
anastasiabolotnikova <bolotnikova.anastasia@gmail.com>
2026-06-22 14:22:26 +0200

    Simplify FSM, add posture task joints weights

--- src/RobogamiController.cpp
+++ src/RobogamiController.cpp
@@ -15,6 +15,23 @@ bool RobogamiController::run()
 void RobogamiController::reset(const mc_control::ControllerResetData & reset_data)
 {
   mc_control::fsm::Controller::reset(reset_data);
+
+  // Low joint weights in posture task for passive DoFs
+  std::map<std::string, double> jointWeights = {{"x_l1rotx", 0.001},
+                                                {"x_l1roty", 0.001},
+                                                {"x_l1top", 0.001},
+                                                {"x_l2rotx", 0.001},
+                                                {"x_l2roty", 0.001},
+                                                {"x_l2top", 0.001},
+                                                {"x_l3rotx", 0.001},
+                                                {"x_l3roty", 0.001},
+                                                {"x_l3top", 0.001},
+                                                {"l1topBase", 0.001},
+                                                {"l2TopMove", 0.001},
+                                                {"l3TopMove", 0.001}};
+                                                
+  getPostureTask("robogami")->jointWeights(jointWeights);
+
 }
 
 
